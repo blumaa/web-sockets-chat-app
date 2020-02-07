bf98ab097101ec39bc2edccb52029544f68a634d
Aaron Blum <blumaa@gmail.com>
2020-02-07 16:56:34 +0100

    chat connection now work

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const socket = require('socket.io')
+const express = require("express");
+const socket = require("socket.io");
 //App Setup
 
 const app = express();
-const server = app.listen(4000, function(){
-  console.log('listening to request of port 4000')
-})
+const server = app.listen(4000, function() {
+  console.log("listening to request of port 4000");
+});
 
 // Static files
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Socket set up
 
 let io = socket(server);
 
-io.on('connection', function(socket){
-  console.log('made socket connection', socket.id)
-})
+io.on("connection", function(socket) {
+  console.log("made socket connection", socket.id);
+
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+});
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,26 @@
 //Make connection
 
 const socket = io.connect('http://localhost:4000')
+
+// Query Dom
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output')
+
+
+//Emit Events
+
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  })
+})
+
+//Listen for events
+
+socket.on('chat', (data)=>{
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
