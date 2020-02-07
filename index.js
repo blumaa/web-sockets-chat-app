7feea246bf95dbc7675ec5f208387388440cf04e
Aaron Blum <blumaa@gmail.com>
2020-02-07 17:17:51 +0100

    final typing messages

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ let io = socket(server);
 io.on("connection", function(socket) {
   console.log("made socket connection", socket.id);
 
-  socket.on("chat", (data) => {
+  socket.on("chat", function(data){
     io.sockets.emit("chat", data);
   });
+
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data)
+  })
 });
--- public/chat.js
+++ public/chat.js
@@ -4,10 +4,11 @@ const socket = io.connect('http://localhost:4000')
 
 // Query Dom
 
-const message = document.getElementById('message');
-const handle = document.getElementById('handle');
-const btn = document.getElementById('send');
-const output = document.getElementById('output')
+let message = document.getElementById('message');
+let handle = document.getElementById('handle');
+let btn = document.getElementById('send');
+let output = document.getElementById('output')
+let feedback = document.getElementById('feedback')
 
 
 //Emit Events
@@ -19,8 +20,17 @@ btn.addEventListener('click', function(){
   })
 })
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value)
+})
+
 //Listen for events
 
-socket.on('chat', (data)=>{
+socket.on('chat', function(data){
+  feedback.innerHTML = ""
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
 })
